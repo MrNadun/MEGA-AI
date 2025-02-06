6121d9e938404d36e85c7d4bd05a5b4435fe70c1
Mr.Nadun <mrnadun878@gmail.com>
2025-02-06 05:28:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '94713961972' ;  // put your number here
+global.pairingNumber = process.env.BOT_NUMBER || '94770584736' ;  // put your number here
 global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
