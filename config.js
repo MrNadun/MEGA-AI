2eae96f26868de9182fa41ebb4f2168c30d88a7f
Mr.Nadun <mrnadun878@gmail.com>
2025-10-07 11:53:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '2347045889973';
+const defaultOwner = '94760405102';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '94760405102' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -22,7 +22,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 global.botname = process.env.BOTNAME || 'AUTOBOB';
 global.mods = []
 global.prems = []
-global.allowed = ['2347045889973', '923051391007']
+global.allowed = ['94760405102', '94760405102']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
